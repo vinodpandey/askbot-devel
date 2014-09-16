8180a3b5c6cfd08b51586b1e5949d9d3df2f385b
Vinod Pandey <vpforyou@gmail.com>
2014-09-17 03:54:40 +0530

    Update cancel.py
    patched code to fix 'User' import error in gunicorn setup

--- askbot/middleware/cancel.py
+++ askbot/middleware/cancel.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponseRedirect
-from askbot.utils.forms import get_next_url
+from django.conf import settings
+
+DEFAULT_NEXT = '/' + getattr(settings, 'ASKBOT_URL')
+def clean_next(next, default = None):
+    if next is None or not next.startswith('/'):
+        if default:
+            return default
+        else:
+            return DEFAULT_NEXT
+    if isinstance(next, str):
+        next = unicode(urllib.unquote(next), 'utf-8', 'replace')
+    next = next.strip()
+    logging.debug('next url is %s' % next)
+    return next
+
+def get_next_url(request, default = None):
+    return clean_next(request.REQUEST.get('next'), default)
+
+
 class CancelActionMiddleware(object):
     def process_view(self, request, view_func, view_args, view_kwargs):
         if 'cancel' in request.REQUEST:
